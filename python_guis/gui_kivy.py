c4ac76d3121c372e1bccea64f8c17a827facf738
Mayeul d'Avezac <2745737+mdavezac@users.noreply.github.com>
2019-07-29 12:14:00 +0100

    Figuring out matplotlib stuff

--- python_guis/gui_kivy.py
+++ python_guis/gui_kivy.py
@@ -2,14 +2,66 @@ from pathlib import Path
 
 from kivy.app import App
 from kivy.config import Config
+from kivy.garden.matplotlib.backend_kivyagg import FigureCanvasKivyAgg
 from kivy.properties import ListProperty, NumericProperty, StringProperty
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.stacklayout import StackLayout
+from matplotlib.pyplot import Figure
 
 Config.set("input", "mouse", "mouse,multitouch_on_demand")
 Config.set("graphics", "width", "800")
 Config.set("graphics", "height", "600")
+
+
+class Matplotlib(FigureCanvasKivyAgg):
+    image = StringProperty(str(Path(__file__).parent / "insects.jpg"))
+    control_points = ListProperty([])
+    node_points = ListProperty([])
+    diameter = 30.0
+
+    def __init__(self, **kwargs):
+        from skimage.io import imread
+
+        self.figure = Figure(tight_layout=True)
+
+        super().__init__(self.figure, **kwargs)
+        self.figure.patch.set_visible(False)
+        self.figure.canvas.mpl_connect("button_release_event", self.add_node)
+        self.image_data = imread(str(self.image), as_gray=True)
+
+    def draw(self):
+        from matplotlib import pyplot as plt
+
+        self.figure.clear()
+        axes = self.figure.add_subplot()
+        axes.imshow(
+            self.image_data, cmap=plt.get_cmap("binary_r")
+        )
+        axes.set_title(
+            "Left click to add a control node.\n"
+            "At least 3 are needed to perform a segmentation."
+        )
+        axes.axis("off")
+        self.draw_control_points(axes)
+        return super().draw()
+
+    def add_node(self, event):
+        if event.inaxes is None:
+            return
+        self.control_points.append((event.xdata, event.ydata))
+        self.draw_control_points(event.inaxes)
+        self.figure.canvas.draw()
+
+    def draw_control_points(self, axes):
+        if len(self.control_points) == 0:
+            return
+
+        x, y = (
+            [x[0] for x in self.control_points] + [self.control_points[0][0]],
+            [x[1] for x in self.control_points] + [self.control_points[0][1]],
+        )
+        axes.plot(x, y, "ro-")
 
 
 class ControlField(StackLayout):
