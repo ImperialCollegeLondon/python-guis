ed7d15d5b9d300792d6fc21c84d6beb34362fc7d
Mayeul d'Avezac <2745737+mdavezac@users.noreply.github.com>
2019-07-30 09:52:21 +0100

    Kivy app seems to work

--- python_guis/gui_kivy.py
+++ python_guis/gui_kivy.py
@@ -3,9 +3,8 @@ from pathlib import Path
 from kivy.app import App
 from kivy.config import Config
 from kivy.garden.matplotlib.backend_kivyagg import FigureCanvasKivyAgg
-from kivy.properties import ListProperty, NumericProperty, StringProperty
+from kivy.properties import ListProperty, NumericProperty
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.stacklayout import StackLayout
 from matplotlib.pyplot import Figure
 
@@ -15,7 +14,6 @@ Config.set("graphics", "height", "600")
 
 
 class Matplotlib(FigureCanvasKivyAgg):
-    image = StringProperty(str(Path(__file__).parent / "insects.jpg"))
     control_points = ListProperty([])
     node_points = ListProperty([])
     diameter = 30.0
@@ -27,31 +25,41 @@ class Matplotlib(FigureCanvasKivyAgg):
 
         super().__init__(self.figure, **kwargs)
         self.figure.patch.set_visible(False)
-        self.figure.canvas.mpl_connect("button_release_event", self.add_node)
-        self.image_data = imread(str(self.image), as_gray=True)
+        self.image_data = imread(
+            str(Path(__file__).parent / "insects.jpg"), as_gray=True
+        )
+
+        def control_point_change(*args):
+            self.draw_control_points(self.figure.axes[0])
+            self.figure.canvas.draw()
+
+        def node_point_change(*args):
+            self.draw_node_points(self.figure.axes[0])
+            self.figure.canvas.draw()
+
+        def add_node(event):
+            if event.inaxes is None:
+                return
+            self.control_points.append((event.xdata, event.ydata))
+
+        self.bind(control_points=control_point_change, node_points=node_point_change)
+        self.figure.canvas.mpl_connect("button_release_event", add_node)
 
     def draw(self):
         from matplotlib import pyplot as plt
 
         self.figure.clear()
         axes = self.figure.add_subplot()
-        axes.imshow(
-            self.image_data, cmap=plt.get_cmap("binary_r")
-        )
+        axes.imshow(self.image_data, cmap=plt.get_cmap("binary_r"))
         axes.set_title(
             "Left click to add a control node.\n"
             "At least 3 are needed to perform a segmentation."
         )
-        axes.axis("off")
+        axes.get_xaxis().set_visible(False)
+        axes.get_yaxis().set_visible(False)
         self.draw_control_points(axes)
+        self.draw_node_points(axes)
         return super().draw()
-
-    def add_node(self, event):
-        if event.inaxes is None:
-            return
-        self.control_points.append((event.xdata, event.ydata))
-        self.draw_control_points(event.inaxes)
-        self.figure.canvas.draw()
 
     def draw_control_points(self, axes):
         if len(self.control_points) == 0:
@@ -63,76 +71,70 @@ class Matplotlib(FigureCanvasKivyAgg):
         )
         axes.plot(x, y, "ro-")
 
+    def draw_node_points(self, axes):
+        if len(self.node_points) == 0:
+            return
+
+        x, y = ([x[0] for x in self.node_points], [x[1] for x in self.node_points])
+        axes.plot(x, y, "b-", linewidth=2)
+
+    def on_segment(self, degree, resolution, sigma):
+        from model import segment_one_image
+        from kivy.clock import Clock
+
+        degree = int(degree)
+        resolution = int(getattr(resolution, "text", resolution))
+        sigma = int(getattr(sigma, "value", sigma))
+
+        self.parent.ids.control_field.disabled = True
+        self.disabled = True
+
+        def reenable(*args):
+            """Enables controls.
+
+            In order to ensure that clicks which happen during the computation are not
+            taken into account, we have to make sure the fields are disabled while these
+            clicks are processed. That means the computation will schedule un-disabling
+            for the frame after it is itself completed.
+            """
+            self.parent.ids.control_field.disabled = False
+            self.disabled = False
+
+        def computation(*args):
+            """Performs contour computations.
+
+            Computations are started in the next frame, *after* disabling the fields
+            becomes visible.
+
+            Note: async/await has landed in kivy dev. Eventually, computations should be
+            performed asynchronously, rather than through a chain of calls to
+            schedule_once.
+            """
+            contour, initial = segment_one_image(
+                nodes=self.control_points,
+                image=self.image_data,
+                degree=degree,
+                resolution=resolution,
+                sigma=sigma,
+            )
+            self.node_points = [(x[0], x[1]) for x in contour]
+            Clock.schedule_once(reenable, 0)
+
+        Clock.schedule_once(computation, 0)
+
 
 class ControlField(StackLayout):
     degree = NumericProperty(1)
-    resolution = NumericProperty(360)
 
     def on_resolution_change(self, textinput):
         try:
-            self.resolution = int(textinput.text)
+            int(textinput.text)
         except ValueError:
             textinput.text = str(self.resolution)
 
 
-class PlayField(FloatLayout):
-    image = StringProperty(str(Path(__file__).parent / "insects.jpg"))
-    diameter = 30.0
-
-    def on_touch_down(self, touch):
-        if touch.button != "left" or not self.collide_point(*touch.pos):
-            return False
-        if super().on_touch_down(touch):
-            return True
-        touch.grab(self)
-        self.parent.control_points.append(touch.pos)
-        return True
-
-    def on_touch_move(self, touch):
-        if touch.grab_current is self:
-            self.parent.control_points[-1] = touch.pos
-            return True
-        return super().on_touch_move(touch)
-
-    def on_touch_up(self, touch):
-        if touch.grab_current is self:
-            touch.ungrab(self)
-            return True
-        return super().on_touch_up(touch)
-
-
 class MainWindow(BoxLayout):
-    control_points = ListProperty([])
-    node_points = ListProperty([])
-
-    def on_segment(self):
-        from numpy import array
-        from skimage.io import imread
-        from .model import segment_one_image
-
-        controls = self.ids.control_field
-        display = self.ids.play_field
-        degree = int(controls.degree)
-        resolution = int(controls.resolution)
-        sigma = int(controls.ids.gaussian_width.value)
-
-        image = imread(self.ids.play_field.image, as_gray=True)
-        factor = array(
-            [
-                image.shape[1] / display.ids.image.width,
-                image.shape[0] / display.ids.image.height,
-            ]
-        )
-        points = (array(self.control_points) - array(display.ids.image.pos)) * factor
-
-        contour, initial = segment_one_image(
-            nodes=points, image=image, degree=degree, resolution=resolution, sigma=sigma
-        )
-        contour = contour / factor + display.ids.image.pos
-        initial = initial / factor + display.ids.image.pos
-        self.node_points = [
-            (contour[i, 0], contour[i, 1]) for i in range(contour.shape[0])
-        ]
+    pass
 
 
 class BeetleApp(App):
