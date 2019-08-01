9142f3fa2450c21743108e80971f530387787a71
Diego <d.alonso-alvarez@imperial.ac.uk>
2019-08-01 13:52:22 +0100

    Updated dependency list.

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ setup(
     author="Research Computing Service, Imperial College London",
     author_email="rcs-support@imperial.ac.uk",
     setup_requires=["pytest-runner"],
-    install_requires=["kivy", "matplotlib", "numpy", "scikit-image", "scipy"],
+    install_requires=["kivy", "matplotlib", "numpy", "scikit-image", "scipy", "jupyter"],
     tests_require=["pytest-cov", "pytest-flake8", "pytest-mypy"],
 )
