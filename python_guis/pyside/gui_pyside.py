66dad0d650e2dfa439037c0552d74842eb653938
Diego <d.alonso-alvarez@imperial.ac.uk>
2021-06-15 16:16:00 +0100

    Add pyside GUI

--- python_guis/pyside/gui_pyside.py
+++ python_guis/pyside/gui_pyside.py
@@ -1,170 +1,185 @@
-import tkinter as tk
-from tkinter import ttk
-from pathlib import Path
+import sys
+
+if sys.platform == "darwin":
+    # There is an issue with pyside2 and MacOS BigSur. This hack sorts it
+    # https://stackoverflow.com/a/64878899/3778792
+    import os
+
+    os.environ["QT_MAC_WANTS_LAYER"] = "1"
 
 import matplotlib.pyplot as plt
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg, NavigationToolbar2QT
 from matplotlib.figure import Figure
+from PySide2 import QtWidgets, QtGui
+from PySide2.QtCore import Qt
+
 
 from python_guis import INSECTS
 from python_guis.model import add_node, segment_one_image
 from skimage.io import imread
 
 
-class BeetlePicker(tk.Tk):
+class PlotArea(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
-        self.title("Beetle Picker")
+        self.setLayout(QtWidgets.QVBoxLayout())
+
+        fig = Figure(tight_layout=True)
+        self.axes = fig.add_subplot()
+        self.axes.get_xaxis().set_visible(False)
+        self.axes.get_yaxis().set_visible(False)
+        self.canvas = FigureCanvasQTAgg(fig)
+        toolbar = NavigationToolbar2QT(self.canvas, self)
+
+        self.layout().addWidget(self.canvas)
+        self.layout().addWidget(toolbar)
+
+    def draw(self):
+        """Redraws the figure, updating its contents."""
+        self.canvas.draw()
+
+
+class Controls(QtWidgets.QWidget):
+    def __init__(self):
+        super(Controls, self).__init__()
+        self.setLayout(QtWidgets.QVBoxLayout())
+
+        # Slider widgets
+        self.label = QtWidgets.QLabel("")
+        self.slider = QtWidgets.QSlider(Qt.Horizontal)
+        self.slider.setTickInterval(1)
+        self.slider.setMinimum(1)
+        self.slider.setMaximum(10)
+        self.slider.valueChanged.connect(self._set_label)
+
+        # Radiobutton widgets
+        self.button_group = QtWidgets.QButtonGroup()
+        buttons = QtWidgets.QHBoxLayout()
+        buttons.addWidget(QtWidgets.QLabel("- Degree: "))
+        for i in [1, 3, 5]:
+            rb = QtWidgets.QRadioButton(f"{i}")
+            buttons.addWidget(rb)
+            self.button_group.addButton(rb)
+
+        self.button_group.buttons()[1].setChecked(True)
+
+        # Resolution widgets
+        resolution = QtWidgets.QHBoxLayout()
+        resolution.addWidget(QtWidgets.QLabel("- Resolution: "))
+        self.resolution_entry = QtWidgets.QLineEdit("360")
+        self.resolution_entry.setValidator(QtGui.QIntValidator())
+        self.resolution_entry.setAlignment(Qt.AlignRight)
+        resolution.addWidget(self.resolution_entry)
+
+        # Buttons
+        self.segment_button = QtWidgets.QPushButton("Perform segmentation")
+        self.reset_button = QtWidgets.QPushButton("Remove all")
+
+        # Add widgets to the layout
+        self.layout().addWidget(self.label)
+        self.layout().addWidget(self.slider)
+        self.layout().addWidget(QtWidgets.QLabel("Spline parameters: "))
+        self.layout().addLayout(buttons)
+        self.layout().addLayout(resolution)
+        self.layout().addWidget(self.segment_button)
+        self.layout().addWidget(self.reset_button)
+        self.layout().addStretch(1)
+
+        self._set_label()
+
+    def _set_label(self):
+        text = f"Gaussian filter width: {self.gauss_width:10}"
+        self.label.setText(text)
+
+    @property
+    def gauss_width(self):
+        return int(self.slider.value())
+
+    @property
+    def degree(self):
+        return int(self.button_group.checkedButton().text())
+
+    @property
+    def resolution(self):
+        return int(self.resolution_entry.text())
+
+
+class MySimpleGUI(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setLayout(QtWidgets.QHBoxLayout())
 
         self.filename = ""
         self.image = None
         self.nodes = []
 
-        # gui variables
-        self.sigma_scale = tk.IntVar(value=1)
-        self.sigma_label = tk.StringVar(value=1)
-        self.spline_resolution = tk.IntVar(value=360)
-        self.spline_degree = tk.IntVar(value=3)
-        self.segment_button = None
-        self.remove_all_segments_button = None
-        self.fig = None
-        self.axes = None
+        self.controls = Controls()
+        self.controls.segment_button.clicked.connect(self.perform_segmentation)
+        self.controls.reset_button.clicked.connect(self.remove_all_segmentations)
+        self.controls.segment_button.setEnabled(False)
+        self.controls.reset_button.setEnabled(False)
+        self.layout().addWidget(self.controls)
 
-        # create the GUI
-        self.create_gui()
+        self.plot = PlotArea()
+        self.layout().addWidget(self.plot)
 
         # read image
         self.read_image()
 
-    def create_gui(self):
-        """Creates the widgets and link them to the GUI variables."""
-        self.columnconfigure(1, weight=1)
-        self.rowconfigure(0, weight=1)
-
-        # The plot
-        self.fig = Figure(tight_layout=True)
-        self.axes = self.fig.add_subplot()
-        self.axes.get_xaxis().set_visible(False)
-        self.axes.get_yaxis().set_visible(False)
-
-        canvas = FigureCanvasTkAgg(self.fig, master=self)
-        canvas.draw()
-        canvas.get_tk_widget().grid(column=1, row=0, sticky=tk.NSEW)
-
-        # The main frame, which will hold all the widgets except the plot
-        mainframe = ttk.Frame(self, width=300)
-        mainframe.grid(column=0, row=0, sticky=tk.NSEW, ipadx=15, ipady=15)
-        mainframe.columnconfigure(1, weight=1)
-
-        # Gaussian filter
-        ttk.Label(mainframe, text="Gaussian filter width: ").grid(
-            row=1, columnspan=2, sticky=tk.NSEW, padx=5, pady=5
-        )
-        ttk.Label(mainframe, textvariable=self.sigma_label).grid(
-            row=2, sticky=tk.E, padx=5, pady=5
-        )
-        ttk.Scale(
-            mainframe,
-            orient=tk.HORIZONTAL,
-            from_=0,
-            to=10,
-            variable=self.sigma_scale,
-            command=lambda *args: self.sigma_label.set(self.sigma_scale.get()),
-        ).grid(row=2, column=1, sticky=tk.NSEW, padx=5, pady=5)
-
-        # Spline
-        ttk.Label(mainframe, text="Spline parameters: ").grid(
-            row=3, columnspan=2, sticky=tk.NSEW, padx=5, pady=5
-        )
-        ttk.Label(mainframe, text="- Degree:").grid(
-            row=4, sticky=tk.NSEW, padx=5, pady=5
-        )
-        degree_frame = ttk.Frame(mainframe)
-        degree_frame.grid(row=4, column=1, sticky=tk.NSEW)
-        for i in [1, 3, 5]:
-            degree_frame.columnconfigure((i - 1) // 2, weight=1)
-            ttk.Radiobutton(
-                degree_frame, text=i, value=i, variable=self.spline_degree
-            ).grid(row=0, column=(i - 1) // 2, sticky=tk.NSEW)
-        ttk.Label(mainframe, text="- Resolution:").grid(
-            row=5, sticky=tk.NSEW, padx=5, pady=5
-        )
-        ttk.Entry(mainframe, textvariable=self.spline_resolution).grid(
-            row=5, column=1, sticky=tk.NSEW, padx=5, pady=5
-        )
-
-        # Perform segmentation
-        self.segment_button = ttk.Button(
-            mainframe,
-            text="Perform segmentation",
-            command=self.perform_segmentation,
-            state=tk.DISABLED,
-        )
-        self.segment_button.grid(row=6, columnspan=2, sticky=tk.NSEW, padx=5, pady=5)
-
-        # Remove data
-        self.remove_all_segments_button = ttk.Button(
-            mainframe,
-            text="Remove all",
-            command=self.remove_all_segmentations,
-            state=tk.DISABLED,
-        )
-        self.remove_all_segments_button.grid(
-            row=7, columnspan=2, sticky=(tk.S, tk.W, tk.E), padx=5, pady=5
-        )
-
     def remove_all_segmentations(self):
         """Removes all segmentations from memory."""
         self.nodes = []
-        self.remove_all_segments_button.configure(state=tk.DISABLED)
-        self.axes.lines.clear()
-        self.axes.get_legend().remove()
-        self.fig.canvas.draw()
+        self.controls.reset_button.setEnabled(False)
+        self.plot.axes.lines.clear()
+        self.plot.axes.get_legend().remove()
+        self.plot.draw()
 
     def add_node(self, event):
         """Adds a node to the plot."""
         if len(self.nodes) == 0:
-            self.axes.lines.clear()
+            self.plot.axes.lines.clear()
 
-        add_node(event, self.nodes, self.fig.canvas)
+        add_node(event, self.nodes, self.plot.canvas)
 
+        self.controls.reset_button.setEnabled(True)
         if len(self.nodes) >= 3:
-            self.segment_button.configure(state=tk.NORMAL)
+            self.controls.segment_button.setEnabled(True)
 
     def redraw(self, segment=None, initial=None):
         """Redraws the axes after making a changes to the data."""
 
         if initial is not None:
-            self.axes.plot(*initial.T, color="blue", label="Initial")
+            self.plot.axes.plot(*initial.T, color="blue", label="Initial")
 
         if segment is not None:
-            self.axes.plot(*segment.T, color="orange", label="Segmented")
+            self.plot.axes.plot(*segment.T, color="orange", label="Segmented")
 
         if segment is not None or initial is not None:
-            self.axes.legend()
+            self.plot.axes.legend()
 
-        self.fig.canvas.draw()
+        self.plot.draw()
 
     def draw(self):
         """Initial drawing of the plot."""
-        self.axes.imshow(self.image, cmap=plt.get_cmap("binary_r"))
-        self.axes.set_title(
+        self.plot.axes.imshow(self.image, cmap=plt.get_cmap("binary_r"))
+        self.plot.axes.set_title(
             "Left click to add a control node.\n"
             "At least 3 are needed to perform a segmentation."
         )
+        self.plot.draw()
 
     def perform_segmentation(self):
         """Gets all the parameters from the widgets and performs the segmentation."""
-        sigma = self.sigma_scale.get()
-        resolution = self.spline_resolution.get()
-        degree = self.spline_degree.get()
+        sigma = self.controls.gauss_width
+        resolution = self.controls.resolution
+        degree = self.controls.degree
 
         segment, initial = segment_one_image(
             self.image, self.nodes, sigma=sigma, resolution=resolution, degree=degree
         )
 
-        self.remove_all_segments_button.configure(state=tk.NORMAL)
-        self.segment_button.configure(state=tk.DISABLED)
+        self.controls.reset_button.setEnabled(True)
+        self.controls.segment_button.setEnabled(False)
 
         self.nodes = []
         self.redraw(segment, initial)
@@ -173,8 +188,13 @@ class BeetlePicker(tk.Tk):
         """Opens the image to segment."""
         self.image = imread(INSECTS, as_gray=True)
         self.draw()
-        self.fig.canvas.mpl_connect("button_release_event", self.add_node)
+        self.plot.canvas.mpl_connect("button_release_event", self.add_node)
 
 
 if __name__ == "__main__":
-    BeetlePicker().mainloop()
+    app = QtWidgets.QApplication([])
+
+    gui = MySimpleGUI()
+    gui.show()
+
+    sys.exit(app.exec_())
